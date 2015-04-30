3f21ca2d401c614d89088c7ffdbdcd614746dd99
Ben Peachey <potherca@gmail.com>
2015-04-30 15:25:05 +0200

    An idea to make things more simple.

--- docs/example.php
+++ docs/example.php
@@ -1,6 +1,5 @@
 <?php
 
-use Gitonomy\Git\Repository;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
 use Potherca\Flysystem\Git\LocalAdapterGitDecorator;
@@ -9,6 +8,20 @@ use Potherca\Flysystem\Adapter\CloneRemoteGitToLocal;
 /* Git Adapter Specific Code */
 $root = '/path/to/repo'
 $gitUrl = 'https://github.com/gitonomy/gitlib.git';
+
+/*
+    Gitonomy and other git-libs all to be simple (or complex) wrapper around
+    commands fed to the Symfony Command class. Seems simpler to just make the
+    call to Command ourselves.
+
+    _Maybe_ we could use the Git class from Composer (to help with URL and AUTH)
+    https://github.com/composer/composer/blob/HEAD/src/Composer/Util/Git.php
+
+    Otherwise we need to parse the URL and do AUTH ourselves.
+
+    Regardless it seems wise to create a "client" class that does the actual
+    connecting, similar to other Adapters (to keep the API familiar for devs).
+*/
 
 /* Generic Adapter/FlySystem Code */
 // =============================================================================
