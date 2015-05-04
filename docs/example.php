7f7c58fac8e2f05a32e1988879318adfff280c98
Ben Peachey <potherca@gmail.com>
2015-05-04 21:41:22 +0200

    Notes preference for using a Git client.

--- docs/example.php
+++ docs/example.php
@@ -1,47 +1,48 @@
 <?php
 
+namespace Potherca\Flysystem;
+
+use Gitonomy\Git\Repository;
 use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
-use Potherca\Flysystem\Git\LocalAdapterGitDecorator;
+use Potherca\Flysystem\Decorator\LocalAdapterGitDecorator;
 use Potherca\Flysystem\Adapter\CloneRemoteGitToLocal;
 use Potherca\Flysystem\Adapter\GitClient;
 
 /* Git Adapter Specific Code */
-$root = '/path/to/repo'
+$root = '/path/to/repo';
 $gitUrl = 'https://github.com/gitonomy/gitlib.git';
 
 /*
-    Gitonomy and other git-libs all to be simple (or complex) wrapper around
-    commands fed to the Symfony Command class. Seems simpler to just make the
-    call to Command ourselves.
+    Using Gitonomy seems better than "doing it ourselves" as it takes other
+    settings and environments into account. It would mean the difference between
+    "works" and "works for me"
 
-    _Maybe_ we could use the Git class from Composer (to help with URL and AUTH)
-    https://github.com/composer/composer/blob/HEAD/src/Composer/Util/Git.php
-
-    Otherwise we need to parse the URL and do AUTH ourselves.
-
-    Regardless it seems wise to create a "client" class that does the actual
-    connecting, similar to other Adapters (to keep the API familiar for devs).
+    Also it seems wise to create a "client" class that does the actual
+    connecting, similar to other Adapters (to keep the API familiar for developers).
 */
 
-/* Generic Adapter/FlySystem Code */
+$repository = new Repository($root);
+$client = new GitClient($repository, $gitUrl);
 // =============================================================================
 $adapter = new Local($root);
-$decoratedAdapter = new LocalAdapterGitDecorator($adapter, $gitUrl);
+$decoratedAdapter = new LocalAdapterGitDecorator($adapter, $client);
 $filesystem = new Filesystem($decoratedAdapter);
 // -----------------------------------------------------------------------------
 //                                  OR
 // -----------------------------------------------------------------------------
-$adapter = new CloneRemoteGitToLocal($root, $gitUrl);
-$filesystem = new Filesystem($adapter);
-// -----------------------------------------------------------------------------
-//                                  OR
-// -----------------------------------------------------------------------------
-$client = new GitClient($gitUrl);
 $adapter = new CloneRemoteGitToLocal($root, $client);
 $filesystem = new Filesystem($adapter);
 // =============================================================================
 
 // Use the Flysystem as you normally would.
+$file = '/composer.json';
+if ($filesystem->has($file)) {
+    $contents = $filesystem->read($file);
+} else {
+    $contents = 'No composer.json present.';
+}
+
+echo $contents;
 
 /*EOF*/
